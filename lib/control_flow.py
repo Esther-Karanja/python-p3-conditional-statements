505a5c7eeb1cb18de0bc538f2c915d8481b492f7
Esther Karanja <est.karanja@gmail.com>
2023-10-02 23:08:25 -0700

    remove repetion in fizzbuzz()

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -22,7 +22,7 @@ def fizzbuzz(num):
         return "FizzBuzz"
     elif num%3==0:
         return  "Fizz"
-    elif num%5==0 or num%5==0:
+    elif num%5==0:
         return "Buzz"
     else:
         return num
